339013b9f7bf2f7015ddce52a13520affc343b9b
KaitlynC123 <kcopeland@eastsideprep.org>
2025-03-03 14:36:57 -0800

    Add files via upload

---
+++ final/js/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var p = prompt('Lego Batman is...');
+    if (p != null) {
+        document.getElementById('my-function').innerHTML = 'The Coolest!';
+    }  
+}
+
+function myFunction() {
+    var p = prompt('Whats your favorite class?');
+    if (p != null) {
+        document.getElementById('my-function').innerHTML = 'Thats so cool!';
+    }  
+}
+
